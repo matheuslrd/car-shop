185b8282e53043179b122709e986fb6bce4ccddf
Matheus Laurindo <matheus104521@gmail.com>
2022-05-30 18:48:47 -0300

    feat: (Requisito 4) cria models, service e controller

---
+++ src/services/Car.ts
@@ -0,0 +1,21 @@
+import { Car, CarSchema } from '../interfaces/CarInterface';
+import CarModel from '../models/Car';
+import MongoService from './MongoService';
+
+class CarService extends MongoService<Car> {
+  constructor(private carModel = new CarModel()) {
+    super(carModel);
+  }
+
+  create = async (obj: Car) => {
+    const parsed = CarSchema.safeParse(obj);
+
+    if (!parsed.success) {
+      return { error: parsed.error };
+    }
+
+    return this.carModel.create(obj);
+  };
+}
+
+export default CarService;
