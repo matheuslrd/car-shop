45f7033f924d328b8b6eac9ea9046d83953e8cf6
Matheus Laurindo <matheus104521@gmail.com>
2022-06-22 20:11:18 -0300

    feat: (Motorcycle) cria rota motorcycle

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,21 @@ import CarController from './controllers/Car';
 import { Car } from './interfaces/CarInterface';
 import CustomRouter from './routes/Router';
 import App from './app';
+import { Motorcycle } from './interfaces/MotorcycleInterface';
+import MotorcycleController from './controllers/MotorcycleController';
 
 const server = new App();
 
 const carController = new CarController();
+const motorcycleController = new MotorcycleController();
 
 const carRouter = new CustomRouter<Car>();
+const motorcycleRouter = new CustomRouter<Motorcycle>();
+
 carRouter.addRoute(carController);
+motorcycleRouter.addRoute(motorcycleController);
 
 server.addRouter(carRouter.router);
+server.addRouter(motorcycleRouter.router);
 
 export default server;
